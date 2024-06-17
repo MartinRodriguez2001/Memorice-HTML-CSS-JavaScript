c772cb3a61d40bda9fc4ab1a104bb0528c75190c
Martin Rodriguez <martin.adolfo.rodriguez90@gmail.com>
2024-06-17 00:24:23 -0400

    tablero de juego

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
-function obtenerCantidad(e){
-    const opcion = document.getElementsByClassName("botonIng");
-    const b8 = opcion[0];
-    const b16 = opcion[1];
-    const b32 = opcion[2];
-}
-
-function lobby(e){
+function lobby(e){//elegir opción y pasar a la siguiente fase
     const elementoCaja = document.getElementById("caja1");
     elementoCaja.remove();
 }
 
-function game(e){
+function tablero(e){// generación de tablero
+    const tablero = document.createElement("div")
+    tablero.className = "tablero"
+    const caja2 = document.getElementById("caja2")
+    caja2.appendChild(tablero)
 
 }
 
+function juego8(e){
+    const botones = docuement.getElementsByClassName("botonIng")
+    const botonJuego8 = botones[0]
+
+}
 document.addEventListener("DOMContentLoaded", (event) => {
     const btnAdd = document.getElementsByClassName("botonIng");
     Array.from(btnAdd).forEach(el => {
-        el.addEventListener("click", lobby);
+        el.addEventListener("click", lobby)
+        el.addEventListener("click", tablero);
     });
 });
 
